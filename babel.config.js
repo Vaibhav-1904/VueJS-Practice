2acea26ffa60774edbb2cc6d556788e1334e6be8
unknown <vaibhav6644@gmail.com>
2021-10-31 20:22:48 +0530

    commit

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
+
+// babel is JavaScript transcompiler which converts Modern JavaScript to
+// crossplatform JavaScript code which runs across all browsers. 
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+      
+    <div>
+
+      <h1>{{description}}</h1>
+
+      <br>
+
+      <img src="./assets/gfg.png" alt="" @load="onImageLoad">
+
+
+    </div>
+  
+</template>
+
+<script>
+
+export default {
+  name:'App',
+  
+  data() {
+    return {
+      isImageLoaded: false
+    }
+  },
+  methods: {
+    onImageLoad () {
+      this.isImageLoaded = true
+    }
+  },
+  watch: {
+    isImageLoaded: function(newValue, oldValue) {
+      console.log("New value is: " + newValue)
+      console.log("Old value is: " + oldValue)
+    }
+  }
+};
+
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
